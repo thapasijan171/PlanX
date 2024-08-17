2a98acef609fda8ef7635c2cf47cd5a10f411b2d
GuWaiY <sijanlamichhanemagar@gmail.com>
2024-08-17 23:37:03 +0530

    add render commit

--- src/components/BottomNav.tsx
+++ src/components/BottomNav.tsx
@@ -13,6 +13,7 @@ import { useResponsiveDisplay } from "../hooks/useResponsiveDisplay";
 import { UserContext } from "../contexts/UserContext";
 
 /**
+ * Component for rendering the bottom navigation bar.
  */
 export const BottomNav = (): JSX.Element | null => {
   const { user } = useContext(UserContext);
